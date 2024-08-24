d4e951c039c5830a416fc314836948f3213d14bf
ramtejvigna <vignaramtejtelagarapu@gmail.com>
2024-08-25 00:10:12 +0530

    Deleted unwanted files

--- ai-mock/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import React from "react";
-import "./index.css";
-
-createRoot(document.getElementById("root")).render(
-    <App />
-);
