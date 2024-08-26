41c3bc583b77e0e92c9682d2eb6c3f43e61326cf
ramtejvigna <vignaramtejtelagarapu@gmail.com>
2024-08-26 19:29:29 +0530

    Added Courses and Updates Profile section

--- mentor-client/src/App.jsx
+++ mentor-client/src/App.jsx
@@ -1,17 +1,25 @@
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
-import { useLocation } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import Sidebar from './components/Sidebar/Sidebar'
+import Home from './components/Home'
+import CourseDetails from './components/Courses/CourseDetails'
+import Courses from './components/Courses'
+import Profile from './components/Profile/Profile'
 
 function App() {
-
 
   return (
     <div className='flex flex-col h-screen bg-gray-900 text-gray-100'>
       <Navbar />
-      <div className='h-screen flex flex-1 overflow-hidden no-scrollbar bg-white'>
-
+      <div className='h-screen flex flex-1 overflow-hidden no-scrollbar'>
         <Sidebar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/courses/:id" element={<CourseDetails />} />
+          <Route exact path="/profile" element={<Profile />} />
+        </Routes>
       </div>
     </div>
   )
--- mentor-client/src/components/Sidebar/Sidebar.jsx
+++ mentor-client/src/components/Sidebar/Sidebar.jsx
@@ -1,69 +1,57 @@
-import React, { useState } from 'react';
-import { FaUser, FaClipboardList, FaBrain, FaBook, FaCog } from 'react-icons/fa';
-import ToggleButton from '@mui/material/ToggleButton';
-import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
-import './index.css';
+import React from 'react';
+import { FaTachometerAlt, FaUser, FaClipboardList, FaEnvelope, FaSearch, FaBrain, FaBook, FaCog } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
-    const [selected, setSelected] = useState(false);
 
     return (
-        <nav className={`bg-[#000] text-gray-100 flex-shrink-0 hidden md:block border-r border-gray-700 transition-all duration-300 ${selected ? 'w-16' : 'w-64'} flex flex-col h-full`}>
-            <ToggleButton
-                value="check"
-                selected={selected}
-                onChange={() => setSelected(!selected)}
-                sx={{ background: 'transparent' }}
-            >
-                <div className='bg-gray-900 w-10 h-10 flex justify-center items-center rounded-lg text-white'>
-                    <ArrowForwardIosOutlinedIcon sx={{ fontSize: '15px' }} className={`${selected ? 'rotate-90' : ''}`} />
-                </div>
-            </ToggleButton>
+        <nav className={`w-64 flex-shrink-0 hidden md:block border-r bg-black`}>
+            <div className="p-4">
+                <SidebarSection title="User">
+                    <Link to='/profile'> <SidebarItem icon={FaUser} label="Profile" /></Link>
 
-            <div className={`${selected ? '' : 'p-4'} flex-grow h-4/5` }>
-                <SidebarSection>
-                    <Link to='/profile'>
-                        <SidebarItem icon={FaUser} label={selected ? "" : "Profile"} />
-                    </Link>
-                    <SidebarItem icon={FaClipboardList} label={selected ? "" : "Students List"} />
+
+
+                    <SidebarItem icon={FaClipboardList} label="Applied" />
                 </SidebarSection>
-                <SidebarSection>
-                    <a href="/interview" className="flex items-center py-2 px-4 hover:bg-[#151515] rounded transition-colors">
+                <SidebarSection title="Internships">
+                    {/* <SidebarItem icon={FaBrain} label="AI Interviews"  /> */}
+
+                    {/* <li>
+        <a href="#" className="flex items-center py-2 px-4 hover:bg-[#151515] rounded transition-colors">
+            <Icon className="w-5 h-5 mr-3" />
+            <span className="font-bold animate-gradient  bg-gradient-to-r from-purple-500 via-pink-500 to-yellow-500">{label}</span>
+        </a>
+        </li> */}
+
+                    <a href="/aimock" className="flex items-center py-2 px-4 hover:bg-[#151515] rounded transition-colors">
                         <FaBrain className="w-5 h-5 mr-3" />
-                        {
-                            !selected ? (
-                                <span className='font-bold animate-gradient bg-gradient-to-r from-yellow-500 via-pink-500 to-violet-500 bg-clip-text text-transparent'>AI Interviewer</span>
-                            ) : (
-                                <span></span>
-                            )
-                        }
+                        <span className='  font-bold animate-gradient bg-gradient-to-r from-yellow-500 via-pink-500 to-violet-500 bg-clip-text text-transparent'>AI Interviewer</span>
                     </a>
                 </SidebarSection>
-                <SidebarSection>
-                    <SidebarItem icon={FaBook} label={selected ? "" : "Courses"} />
+                <SidebarSection title="Courses">
+                    <Link to="/courses">
+                        <SidebarItem icon={FaBook} label="Courses" />
+                    </Link>
                 </SidebarSection>
-            </div>
-
-            {/* Bottom section */}
-            <div className={`${selected ? '' : 'p-4'}`}>
-                <SidebarSection>
-                    <SidebarItem icon={FaCog} label={selected ? "" : "Settings"} />
+                <SidebarSection title="Settings">
+                    <SidebarItem icon={FaCog} label="Settings" />
                 </SidebarSection>
             </div>
         </nav>
     );
 };
 
-const SidebarSection = ({ children }) => (
+const SidebarSection = ({ title, children }) => (
     <div className="mb-6">
+        <h6 className="text-gray-250 text-sm font-semibold mb-2">{title}</h6>
         <ul>{children}</ul>
     </div>
 );
 
-const SidebarItem = ({ icon: Icon, label }) => (
+const SidebarItem = ({ icon: Icon, label, isDarkMode }) => (
     <li>
-        <a href="#" className="flex items-center py-2 px-4 hover:bg-[#151515] rounded transition-colors">
+        <a href="#" className={`flex items-center py-2 px-4 ${isDarkMode ? 'hover:bg-[#b9b8b8]' : 'hover:bg-[#121010]'} rounded transition-colors`}>
             <Icon className="w-5 h-5 mr-3" />
             <span className='text-gray-400'>{label}</span>
         </a>
