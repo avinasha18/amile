48fa4499045c5b8fc2ed4d9cb89bd0b088b65f45
ramtejvigna <vignaramtejtelagarapu@gmail.com>
2024-08-31 22:43:01 +0530

    Change Database link

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/amile', {
+        await mongoose.connect('mongodb+srv://vignaramtejtelagarapu:vzNsqoKpAzHRdN9B@amile.auexv.mongodb.net/?retryWrites=true&w=majority&appName=Amile', {
         });
         console.log('Connected to MongoDB');
         
